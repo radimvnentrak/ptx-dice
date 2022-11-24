37af382b48041acbff843615ed53b0d679f66cc6
radimvnentrak <115144523+radimvnentrak@users.noreply.github.com>
2022-11-24 11:41:26 +0100

    oprava 2

--- main.ts
+++ main.ts
@@ -1,14 +1,18 @@
 let number = 5
-whaleysans.showNumber(number)
-input.onButtonPressed(Button.A, function () {
-    if (number > 0) {
-        number -= 1
-    }
+
+basic.forever(function () {
+
     whaleysans.showNumber(number)
-})
-input.onButtonPressed(Button.B, function () {
-    if (number <10) {
-        number += 1
+    if (input.buttonIsPressed(Button.A)) {
+        if (number > 0) {
+            number -= 1
+        }
     }
-whaleysans.showNumber(number)
 })
+
+// input.buttonIsPressed(Button.B, function () {
+//     if (number < 10) {
+//         number += 1
+//     }
+//     whaleysans.showNumber(number)
+// })
